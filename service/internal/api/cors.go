68fc49551cf7e8d70b45a41093f90bd32d1ad5ed
gus <angusgyoung@gmail.com>
2021-09-09 16:43:01 +0000

    Add read cors allowed origins from env

--- service/internal/api/cors.go
+++ service/internal/api/cors.go
@@ -1,10 +1,22 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var CORS_ALLOW_ORIGIN = os.Getenv("CORS_ALLOW_ORIGIN")
 
 func CORS() gin.HandlerFunc {
+
+    var corsAllowedOrigin = "http://localhost:8080"
+    if CORS_ALLOW_ORIGIN != "" {
+        corsAllowedOrigin = CORS_ALLOW_ORIGIN
+    }
+
     return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", corsAllowedOrigin)
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
